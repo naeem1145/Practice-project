9d4162893c76d11c4e5950ac720339a343aa9ed0
Naeem Hassan tonmoy <tonmoy.free10@gmail.com>
2023-10-28 15:28:04 +0600

    contact form done & fix navbar issue

--- responsive-startup/src/main.js
+++ responsive-startup/src/main.js
@@ -3,12 +3,12 @@ let closeBtn = document.querySelector('.bi-x-lg');
 let menu = document.querySelector('.navbar__menu');
 
 openBtn.addEventListener('click', () => {
-    openBtn.classList.toggle('hide');
-    closeBtn.classList.toggle('hide');
+    openBtn.classList.toggle("hide");
+    closeBtn.classList.toggle("hide");
     menu.classList.toggle('hidden');
 });
 
- closeBtn.addEventListener("click", () => {
+closeBtn.addEventListener("click", () => {
     openBtn.classList.toggle("hide");
     closeBtn.classList.toggle("hide");
     menu.classList.toggle('hidden');
